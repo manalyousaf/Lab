9e8130072670ffa68687198c7b4fdad52117ba05
manalyousaf <116888523+manalyousaf@users.noreply.github.com>
2022-10-30 22:29:55 +0500

    Update postlabtask10.c

--- postlabtask10.c
+++ postlabtask10.c
@@ -53,6 +53,39 @@ int main()
 			}
 			printf(" \nboarding pass: \n your seat number is : FC%d\n its in the first class \n thank you", seat);
 			break;
+		case 2:
+			//displaying chart
+			printf("\nSeating chart of economy class is:\n");
+			for (int i = 30; i < 100; i++)
+			{
+				printf("%d ", array[i]);
+			}
+			printf("\nSelect seat number (31-100):");
+			do //input validation
+			{
+				scanf_s("%d", &seat);
+				if (seat > 100 || seat < 31) //if input is wrong
+				{
+					printf("You entered wrong input!\n");
+				}
+				if (seat < 101 && seat > 30)
+				{
+					if (array[seat - 1] == 1) //if seat is already booked	
+					{
+						printf("Already booked please select any other\n");
+					}
+				}
+			} while (seat > 100 || seat < 31 || array[seat - 1] == 1);
+			array[seat - 1] = 1;
+			//modified chart
+			printf("Seating chart of economy class is \n");
+			for (int i = 30; i < 100; i++)
+			{
+				printf("%d ", array[i]);
+			}
+			printf(" \nboarding pass: \n your seat number is : EC%d\n its in the economy class \n thank you", seat);
+			break;
+			//if sentinel value is entered
 		case 3:
 			printf("exit!");
 			break;
